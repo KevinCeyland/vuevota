4e468bc3c551e6612624d48477068ce286802e6a
KevinCeyland <kevin-78440@hotmail.fr>
2022-02-09 11:20:44 +0100

    Installation de axios

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios"
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+Vue.prototype.$axios = axios
+Vue.prototype.$store = store
+
+
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
